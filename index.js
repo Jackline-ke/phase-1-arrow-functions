7002213d2ba82b8f296d3a496000d315e94a80ec
Jackline-ke <antoninahodhiambo@gmail.com>
2023-03-21 04:54:50 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = (number1, number2) => 2000/100;
+//console.log(divide(2000, 100));
+
+const square = (x) => (2 ** 2);
+//const square = (number) => (Math.pow(2,2));
+
+const add = (a, b) => (3 + 4);
